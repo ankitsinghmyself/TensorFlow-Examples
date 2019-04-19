812e481ea996660d1c2960c50e109c6f114db757
Ankit Singh <43289790+ankitsinghmyself@users.noreply.github.com>
2019-04-19 18:51:57 +0530

    Update hello.py

--- Examples/hello.py
+++ Examples/hello.py
@@ -5,6 +5,13 @@ Author: Ankit Singh
 Project: https://github.com/ankitsinghmyself/TensorFlow/
 '''
 import tensorflow as tf
+# Simple hello world using TensorFlow
+
+# Create a Constant op
+# The op is added as a node to the default graph.
+# The value returned by the constructor represents the output of the Constant op.
 hello = tf.constant('Hello, TensorFlow!')
+#start session tf
 sess = tf.Session()
+# Run the op
 print(sess.run(hello))
