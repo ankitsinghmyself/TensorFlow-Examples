e9301aad11d536ec27b58e67950f99f74ffd5cd0
Ankit Singh <43289790+ankitsinghmyself@users.noreply.github.com>
2019-04-19 18:49:32 +0530

    Update hello.py

--- Examples/hello.py
+++ Examples/hello.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-
+'''
+HelloWorld example using TensorFlow library.
+Author: Ankit Singh
+Project: https://github.com/ankitsinghmyself/TensorFlow/
+'''
 import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
